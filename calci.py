57c9890f7ae37fdfeec893866a165162182600b1
Minny-05 <kamallameenakshi2005@gmail.com>
2026-02-05 11:14:21 +0530

    Add basic calculator functionality in calci.py

---
+++ calci.py
@@ -0,0 +1,12 @@
+n1 = int(input("Enter first number: "))
+n2 = int(input("Enter second number: "))
+
+r1 = n1 + n2
+r2 = n1 - n2
+r3 = n1 * n2
+r4 = n1 / n2
+
+print(r1)
+print(r2)
+print(r3)
+print(r4)
